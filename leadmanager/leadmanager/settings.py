5be7628f2192c58068d01524067496250aa46656
ishwardev <53650288+NiranjanT95@users.noreply.github.com>
2023-05-23 15:59:29 +0530

    remove postgres

--- leadmanager/leadmanager/settings.py
+++ leadmanager/leadmanager/settings.py
@@ -82,17 +82,23 @@ WSGI_APPLICATION = 'leadmanager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'lead_manager_forked',
+#         'USER': 'root',
+#         'PASSWORD': 'admin123',
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'lead_manager_forked',
-        'USER': 'root',
-        'PASSWORD': 'admin123',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
